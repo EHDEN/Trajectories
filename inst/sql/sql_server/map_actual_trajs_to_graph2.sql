6243bac9b0cec442c6bf4dae71b154ee6bdb9cb4
Kadri <32059518+Kadri0@users.noreply.github.com>
2020-09-11 10:12:38 +0300

    Changed sql syntax for creating tables

--- inst/sql/sql_server/map_actual_trajs_to_graph2.sql
+++ inst/sql/sql_server/map_actual_trajs_to_graph2.sql
@@ -4,7 +4,7 @@
 IF OBJECT_ID('@resultsSchema.@prefiXgraph_events', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefiXgraph_events;
 
-CREATE TABLE @resultsSchema.@prefiXgraph_events as
+--CREATE TABLE @resultsSchema.@prefiXgraph_events as
     WITH ee AS (
     SELECT DISTINCT cohort_id,dgn,date from @resultsSchema.@prefiXevents -- here is DISTINCT only to avoid duplicate events from two tables: drugs + drug_eras
     where dgn in (
@@ -16,6 +16,7 @@ CREATE TABLE @resultsSchema.@prefiXgraph_events as
 SELECT ee.cohort_id,
        ee.dgn as e,
        ee2.cohort_day
+INTO @resultsSchema.@prefiXgraph_events
 FROM ee
     LEFT JOIN (
     SELECT ee.cohort_id,
@@ -34,7 +35,7 @@ ORDER BY cohort_id,cohort_day;
 IF OBJECT_ID('@resultsSchema.@prefiXgraph_event_pairs', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefiXgraph_event_pairs;
 
-CREATE TABLE @resultsSchema.@prefiXgraph_event_pairs AS
+--CREATE TABLE @resultsSchema.@prefiXgraph_event_pairs AS
     SELECT  a.cohort_id,
             a.cohort_day as e1_cohort_day,
             b.cohort_day as e2_cohort_day,
@@ -42,6 +43,7 @@ CREATE TABLE @resultsSchema.@prefiXgraph_event_pairs AS
             b.e as e2_concept_id,
             CASE WHEN (a.e=@eventid) THEN 1 ELSE 0 END AS E1_IS_INDEXEVENT,
             CASE WHEN (b.e=@eventid) THEN 1 ELSE 0 END AS E2_IS_INDEXEVENT
+    INTO @resultsSchema.@prefiXgraph_event_pairs
     FROM
            @resultsSchema.@prefiXgraph_events a
            JOIN  @resultsSchema.@prefiXgraph_events b -- full join!
