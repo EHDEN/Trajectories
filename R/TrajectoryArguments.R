EHDEN/Trajectories

commit 357892cbe1959c2429ec655a24ecbae950c8ac81

Changed sql syntax for creating tables #5

inst/sql/sql_server/1CohortCC.sql
@@ -32,7 +32,7 @@ INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES ('Creating @resultsSchema
 IF OBJECT_ID('@resultsSchema.@prefiXmycohort', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefiXetcohort;
 
-CREATE TABLE @resultsSchema.@prefiXetcohort AS
+--CREATE TABLE @resultsSchema.@prefiXetcohort AS
     SELECT
            ROW_NUMBER() OVER (
             ORDER BY cohort.subject_id, cohort.cohort_start_date
@@ -44,6 +44,7 @@ CREATE TABLE @resultsSchema.@prefiXetcohort AS
            YEAR(p.birth_datetime) AS year_of_birth,
            cohort.cohort_start_date AS cohort_start_date,
            cohort.cohort_end_date AS cohort_end_date
+    INTO @resultsSchema.@prefiXetcohort
     FROM
         @cohortTableSchema.@cohortTable AS cohort
         LEFT JOIN @cdmDatabaseSchema.person p on cohort.subject_id=p.person_id
@@ -66,13 +67,14 @@ INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES ('Creating @resultsSchema
 
 IF OBJECT_ID('@resultsSchema.@prefiXevents', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefiXevents;
-CREATE TABLE @resultsSchema.@prefiXevents AS
+--CREATE TABLE @resultsSchema.@prefiXevents AS
 
     -- conditions
     SELECT
       c.cohort_id                  AS cohort_id,
       e.condition_concept_id       AS dgn,
       MIN(e.condition_start_date)  AS date -- This is min date per one cohort (for patients with multiple cohorts, there are several min dates)
+    INTO @resultsSchema.@prefiXevents
     FROM @cdmDatabaseSchema.condition_occurrence e
     INNER JOIN @resultsSchema.@prefiXetcohort c ON e.person_id=c.person_id {@daysBeforeIndexDate == Inf} ? {} : { AND DATEADD(day,@daysBeforeIndexDate,e.condition_start_date)>=c.cohort_start_date } AND e.condition_start_date<=c.cohort_end_date
     WHERE
@@ -183,10 +185,11 @@ INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES ('Creating @resultsSchema
 IF OBJECT_ID('@resultsSchema.@prefiXsingle_event_cohorts', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefiXsingle_event_cohorts;
 
-CREATE TABLE  @resultsSchema.@prefiXsingle_event_cohorts AS
+--CREATE TABLE  @resultsSchema.@prefiXsingle_event_cohorts AS
     SELECT
         cohort_id,
         COUNT(dgn) AS no_events
+    INTO @resultsSchema.@prefiXsingle_event_cohorts
     FROM @resultsSchema.@prefiXevents
     GROUP BY cohort_id
     HAVING COUNT(dgn)=1;
@@ -210,10 +213,11 @@ INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES ('Creating @resultsSchema
 
 IF OBJECT_ID('@resultsSchema.@prefiXevents_cohort', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefiXevents_cohort;
-CREATE TABLE  @resultsSchema.@prefiXevents_cohort AS
+--CREATE TABLE  @resultsSchema.@prefiXevents_cohort AS
     SELECT e.*,
            p.gender,
            p.year_of_birth
+    INTO @resultsSchema.@prefiXevents_cohort
     FROM  @resultsSchema.@prefiXevents AS e
         INNER JOIN  @resultsSchema.@prefiXetcohort AS p
             ON e.cohort_id = p.cohort_id;
@@ -245,9 +249,10 @@ INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES ('Creating @resultsSchema
 IF OBJECT_ID('@resultsSchema.@prefiXevent_counts', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefiXevent_counts;
 
-CREATE TABLE  @resultsSchema.@prefiXevent_counts AS
+--CREATE TABLE  @resultsSchema.@prefiXevent_counts AS
 SELECT dgn,
        COUNT(cohort_id)  AS event_counts
+INTO @resultsSchema.@prefiXevent_counts
 FROM @resultsSchema.@prefiXevents_cohort
 GROUP BY dgn;
 
@@ -262,7 +267,7 @@ INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES ('Creating @resultsSchema
 IF OBJECT_ID('@resultsSchema.@prefiXpairs', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefiXpairs;
 
-CREATE TABLE  @resultsSchema.@prefiXpairs AS
+--CREATE TABLE  @resultsSchema.@prefiXpairs AS
     SELECT  a.cohort_id,
             a.gender,
 
@@ -310,6 +315,7 @@ CREATE TABLE  @resultsSchema.@prefiXpairs AS
             DATEDIFF(DAY,
                     a.date,
                     b.date) AS diff_days
+    INTO @resultsSchema.@prefiXpairs
     FROM
            @resultsSchema.@prefiXevents_cohort a
            JOIN  @resultsSchema.@prefiXevents_cohort b -- full join!
@@ -340,12 +346,13 @@ INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES ('Creating @resultsSchema
 IF OBJECT_ID('@resultsSchema.@prefiXD1D2_model', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefiXD1D2_model;
 
-CREATE TABLE @resultsSchema.@prefiXD1D2_model as
+--CREATE TABLE @resultsSchema.@prefiXD1D2_model as
     SELECT
             event1_concept_id,
             event2_concept_id,
             ROUND(AVG(diff_days)) AS AVG_NUMBER_OF_DAYS_BETWEEN_EVENTS,
             count(*) AS EVENT1_EVENT2_COHORT_COUNT
+    INTO @resultsSchema.@prefiXD1D2_model
     FROM
      @resultsSchema.@prefiXpairs
     GROUP BY
@@ -410,7 +417,7 @@ INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES ('Creating @resultsSchema
 IF OBJECT_ID('@resultsSchema.@prefiXd1d2_summary', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefiXd1d2_summary;
 
-CREATE TABLE @resultsSchema.@prefiXd1d2_summary as
+--CREATE TABLE @resultsSchema.@prefiXd1d2_summary as
     SELECT
       a.event1_concept_id AS event1_concept_id,
       a.event2_concept_id AS event2_concept_id,
@@ -418,6 +425,7 @@ CREATE TABLE @resultsSchema.@prefiXd1d2_summary as
       age,
       discharge_time,
       count(*) AS cohort_count -- as there can't be duplicate event1_concept_id->event2_concept_id event pairs per cohorts, count(*) in this table gives the cohort count
+    INTO @resultsSchema.@prefiXd1d2_summary
     FROM @resultsSchema.@prefiXpairs a
       INNER JOIN @resultsSchema.@prefiXD1D2_model b ON a.event1_concept_id=b.event1_concept_id AND a.event2_concept_id=b.event2_concept_id -- limit the table to event pairs only that are going to be analyzed
     GROUP BY a.event1_concept_id, a.event2_concept_id, gender, age, discharge_time
@@ -434,12 +442,13 @@ INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES ('Creating @resultsSchema
 IF OBJECT_ID('@resultsSchema.@prefiXsummary', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefiXsummary;
 
-CREATE TABLE @resultsSchema.@prefiXsummary as
+--CREATE TABLE @resultsSchema.@prefiXsummary as
     SELECT
       gender,
       age,
       discharge_time,
       count(DISTINCT cohort_id) AS cohort_count
+    INTO @resultsSchema.@prefiXsummary
     FROM @resultsSchema.@prefiXpairs
     GROUP BY gender, age, discharge_time
     ORDER BY gender, age, discharge_time;
@@ -455,13 +464,14 @@ INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES ('Creating @resultsSchema
 IF OBJECT_ID('@resultsSchema.@prefiXd1_summary', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefiXd1_summary;
 
-CREATE TABLE @resultsSchema.@prefiXd1_summary as
+--CREATE TABLE @resultsSchema.@prefiXd1_summary as
     SELECT
       a.event1_concept_id,
       gender,
       age,
       discharge_time,
       count(DISTINCT cohort_id) AS cohort_count
+    INTO @resultsSchema.@prefiXd1_summary
     FROM @resultsSchema.@prefiXpairs a
     INNER JOIN (SELECT DISTINCT event1_concept_id FROM @resultsSchema.@prefiXD1D2_model) b ON a.event1_concept_id=b.event1_concept_id -- limit the table to event pairs only that are going to be analyzed
     GROUP BY a.event1_concept_id, gender, age, discharge_time;
@@ -475,13 +485,14 @@ INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES ('Creating @resultsSchema
 
 IF OBJECT_ID('@resultsSchema.@prefiXd2_summary', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefiXd2_summary;
-CREATE TABLE @resultsSchema.@prefiXd2_summary as
+--CREATE TABLE @resultsSchema.@prefiXd2_summary as
     SELECT
       a.event2_concept_id,
       gender,
       age,
       discharge_time,
       count(DISTINCT cohort_id) AS cohort_count
+    INTO @resultsSchema.@prefiXd2_summary
     FROM @resultsSchema.@prefiXpairs a
     INNER JOIN (SELECT DISTINCT event2_concept_id FROM @resultsSchema.@prefiXD1D2_model) b ON a.event2_concept_id=b.event2_concept_id -- limit the table to event pairs only that are going to be analyzed
     GROUP BY a.event2_concept_id, gender, age, discharge_time;

R/createEventPairsTable.R
@@ -75,6 +75,9 @@ createEventPairsTable<-function(connection,
                                                   addDeaths = trajectoryAnalysisArgs$addDeaths
   )
 
+  fileConn<-file("creaate_table_as_CohortCC.txt")
+  writeLines(RenderedSql, fileConn)
+  close(fileConn)
 
   DatabaseConnector::executeSql(connection, RenderedSql)
 

extras/CodeToRun.R
@@ -19,16 +19,18 @@ connectionDetails = createConnectionDetails(dbms = 'postgresql',#  e.g. oracle, 
 # Setting database parameters - CHANGE ACCORDING TO YOUR DATABASE:
 # library(stringi)
 trajectoryLocalArgs <- Trajectories::createTrajectoryLocalArgs(oracleTempSchema = "temp_schema",
-                                                               prefixForResultTableNames = "sr_", # Alternatively, you could use this to randomly generate the prefix (requires library(stringi) to be loaded): paste0(  if(!is.null(attr(connectionDetails,'user'))) substr(USER,1,2), stri_rand_strings(1, 2, pattern = "[A-Za-z]"), sep="_")
-                                                               cdmDatabaseSchema = 'ohdsi_cdm',
-                                                               vocabDatabaseSchema = 'ohdsi_vocab',
+
+                                                            prefixForResultTableNames = "sr_", # Alternatively, you could use this to randomly generate the prefix (requires library(stringi) to be loaded): paste0(  if(!is.null(attr(connectionDetails,'user'))) substr(USER,1,2), stri_rand_strings(1, 2, pattern = "[A-Za-z]"), sep="_")
+                                                             cdmDatabaseSchema = 'ohdsi_cdm',
+                                                              vocabDatabaseSchema = 'ohdsi_vocab',
                                                                resultsSchema = 'ohdsi_temp',
-                                                               sqlRole = F, # You may always use 'F'. Setting specific role might be useful in PostgreSQL when you want to create tables by using specific role so that the others also see the results. However, then you must ensure that this role has permissions to read from all necessary schemas and also create tables to resultsSchema
+                                                              sqlRole = F, # You may always use 'F'. Setting specific role might be useful in PostgreSQL when you want to create tables by using specific role so that the others also see the results. However, then you must ensure that this role has permissions to read from all necessary schemas and also create tables to resultsSchema
                                                                cohortTableSchema= 'ohdsi_temp',
                                                                cohortTable='cohort',
                                                                cohortId=1,
                                                                mainOutputFolder='/Users/sulevr/temp',
                                                                databaseHumanReadableName='RITA MAITT', #Use something short. It will be added to the titles of the graph.
+
                                                                cohortSqlFile='example_cohort_RA.sql')
 
 

inst/sql/sql_server/7DirectionCounts.sql
@@ -3,12 +3,13 @@
 IF OBJECT_ID('@resultsSchema.@prefixcohorts_with_d1', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefixcohorts_with_d1;
 
-CREATE TABLE @resultsSchema.@prefixcohorts_with_d1 as
+--CREATE TABLE @resultsSchema.@prefixcohorts_with_d1 as
     SELECT
                cohort_id as cohort_id,
                dgn,
                date,
                age
+        INTO @resultsSchema.@prefixcohorts_with_d1
         FROM
              @resultsSchema.@prefixevents_cohort
         WHERE
@@ -20,7 +21,7 @@ CREATE TABLE @resultsSchema.@prefixcohorts_with_d1 as
 IF OBJECT_ID('@resultsSchema.@prefixcohorts_with_d1d2', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefixcohorts_with_d1d2;
 
-CREATE TABLE @resultsSchema.@prefixcohorts_with_d1d2 as
+--CREATE TABLE @resultsSchema.@prefixcohorts_with_d1d2 as
     SELECT
                a.cohort_id as cohort_id,
                b.dgn as event1_concept_id,
@@ -28,6 +29,7 @@ CREATE TABLE @resultsSchema.@prefixcohorts_with_d1d2 as
                b.date as date1,
                a.date as date2,
                a.age as event1_age
+        INTO @resultsSchema.@prefixcohorts_with_d1d2
         FROM
              @resultsSchema.@prefixevents_cohort a
                  JOIN @resultsSchema.@prefixcohorts_with_d1 b

commit a0a68d82136a11d39bfd323ef2cef2603ed078c7

* Fixed one bug when creating @resultsSchema.@prefiXevents table (was introduced in commit 357892cbe1959c2429ec655a24ecbae950c8ac81 in #5). * Improved database logging in createEventPairs() method - now also adds table sizes to debug.

R/TrajectoryArguments.R
@@ -58,7 +58,7 @@ createTrajectoryAnalysisArgs <- function(minimumDaysBetweenEvents,
 #' @param cohortTableSchema Schema where cohort table is located
 #' @param cohortTable Name of the cohort table in cohortTableSchema
 #' @param cohortId ID of the cohort (in cohortTable) that will be used for the analysis (default value is 1)
-#' @param inputFolder Full path to input folder that contains SQL file for cohort definition (SQL Server format) and optionally also trajectoryAnalysisArgs.json. You can use built-in folders of this package such as: inputFolder=system.file("extdata", "RA", package = "Trajectories") which is also the default value
+#' @param inputFolder Full path to input folder that contains SQL file for cohort definition (SQL Server format) and optionally also trajectoryAnalysisArgs.json. You can use built-in folders of this package such as: inputFolder=system.file("extdata", "RA", package = "Trajectories") which is also the default value. In case your cohort data already exists in the database and you do not need to build it from scratch, set the value to FALSE.
 #' @param mainOutputFolder The output folder path. This is the folder where the final results are produced into. Use full path and do NOT add trailing slash! The folder must already exist. Default value is the default working directory.
 #' @param databaseHumanReadableName In the future, it will be added to the titles of the graph to indicate what data is this. Use something short. Currently this parameter is not used.
 #'
@@ -80,9 +80,11 @@ createTrajectoryLocalArgs <- function(cdmDatabaseSchema,
                                       databaseHumanReadableName='My database') {
 
   #Sanity checks
-  if (!dir.exists(inputFolder)) stop(paste0("ERROR in createTrajectoryLocalArgs(): inputFolder '",inputFolder,"' does not exist."))
+  if(!is.logical(inputFolder)) {
+    if (!dir.exists(inputFolder)) stop(paste0("ERROR in createTrajectoryLocalArgs(): inputFolder '",inputFolder,"' does not exist."))
+    if (!file.exists(file.path(inputFolder,'cohort.sql'))) stop(paste0("ERROR in createTrajectoryLocalArgs(): there is no 'cohort.sql' file in inputFolder '",inputFolder,"'."))
+  }
   if (!dir.exists(mainOutputFolder)) stop(paste0("ERROR in createTrajectoryLocalArgs(): mainOutputFolder '",mainOutputFolder,"' does not exist."))
-  if (!file.exists(file.path(inputFolder,'cohort.sql'))) stop(paste0("ERROR in createTrajectoryLocalArgs(): there is no 'cohort.sql' file in inputFolder '",inputFolder,"'."))
   #if (!file.exists(paste0(inputFolder,'/trajectoryAnalysisArgs.json'))) stop(paste0("ERROR in createTrajectoryLocalArgs(): there is no 'trajectoryAnalysisArgs.json' file in inputFolder '",inputFolder,"'."))
 
   value <- list(cdmDatabaseSchema=cdmDatabaseSchema,vocabDatabaseSchema=vocabDatabaseSchema,

inst/sql/sql_server/1CohortCC.sql
@@ -19,6 +19,11 @@ CREATE TABLE @resultsSchema.@prefiXdebug (
 
 INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES ('Debug table created');
 
+-- First, add some data about the size of the database
+INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES (CONCAT('Database size: There are ',CAST((SELECT COUNT(*) FROM @cdmDatabaseSchema.person) AS VARCHAR),' rows in @cdmDatabaseSchema.person'));
+INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES (CONCAT('Database size: There are ',CAST((SELECT COUNT(*) FROM @cdmDatabaseSchema.condition_occurrence) AS VARCHAR),' rows in @cdmDatabaseSchema.condition_occurrence'));
+
+
 
 ---------------------------------------------------------------------------------------
 -- Create a temporary "etcohort" table - it is basically the same table & content as the cohort in OMOP cohorts table but
@@ -58,6 +63,8 @@ IF OBJECT_ID('@resultsSchema.@prefiXetcohort', 'U') IS NOT NULL
         -- AND person_id IN (SELECT DISTINCT person_id FROM @cdmDatabaseSchema.condition_occurrence where condition_source_value like 'C50%');
 ;
 
+INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES (CONCAT('..done. There are ',CAST((SELECT COUNT(*) FROM @resultsSchema.@prefiXetcohort) AS VARCHAR),' rows in @resultsSchema.@prefiXetcohort'));
+
 ---------------------------------------------------------------------------------------------
 -- Create an events table.
 -- Keeping only the first occurrence of each event type for each cohort
@@ -69,12 +76,15 @@ IF OBJECT_ID('@resultsSchema.@prefiXevents', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefiXevents;
 --CREATE TABLE @resultsSchema.@prefiXevents AS
 
+    SELECT * FROM
+
+    (
     -- conditions
     SELECT
       c.cohort_id                  AS cohort_id,
       e.condition_concept_id       AS dgn,
       MIN(e.condition_start_date)  AS date -- This is min date per one cohort (for patients with multiple cohorts, there are several min dates)
-    INTO @resultsSchema.@prefiXevents
+    
     FROM @cdmDatabaseSchema.condition_occurrence e
     INNER JOIN @resultsSchema.@prefiXetcohort c ON e.person_id=c.person_id {@daysBeforeIndexDate == Inf} ? {} : { AND DATEADD(day,@daysBeforeIndexDate,e.condition_start_date)>=c.cohort_start_date } AND e.condition_start_date<=c.cohort_end_date
     WHERE
@@ -173,7 +183,11 @@ IF OBJECT_ID('@resultsSchema.@prefiXevents', 'U') IS NOT NULL
       e.death_datetime IS NOT NULL
     GROUP BY c.cohort_id
 
+    ) SSS INTO @resultsSchema.@prefiXevents
+
 ;
+
+INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES (CONCAT('..done. There are ',CAST((SELECT COUNT(*) FROM @resultsSchema.@prefiXevents) AS VARCHAR),' rows in @resultsSchema.@prefiXevents'));
 
 
 ---------------------------------------------------------------------------------------------
@@ -194,6 +208,8 @@ IF OBJECT_ID('@resultsSchema.@prefiXsingle_event_cohorts', 'U') IS NOT NULL
     GROUP BY cohort_id
     HAVING COUNT(dgn)=1;
 
+INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES (CONCAT('..done. There are ',CAST((SELECT COUNT(*) FROM @resultsSchema.@prefiXsingle_event_cohorts) AS VARCHAR),' rows in @resultsSchema.@prefiXsingle_event_cohorts'));
+
 DELETE FROM @resultsSchema.@prefiXevents
 WHERE cohort_id IN (SELECT cohort_id FROM @resultsSchema.@prefiXsingle_event_cohorts);
 
@@ -203,6 +219,8 @@ WHERE cohort_id IN (SELECT cohort_id FROM @resultsSchema.@prefiXsingle_event_coh
 
 IF OBJECT_ID('@resultsSchema.@prefiXsingle_event_cohorts', 'U') IS NOT NULL
   DROP TABLE @resultsSchema.@prefiXsingle_event_cohorts;
+
+
 
 
 ---------------------------------------------------------------------------------------------
@@ -218,9 +236,11 @@ IF OBJECT_ID('@resultsSchema.@prefiXevents_cohort', 'U') IS NOT NULL
            p.gender,
            p.year_of_birth
     INTO @resultsSchema.@prefiXevents_cohort
-    FROM  @resultsSchema.@prefiXevents AS e
-        INNER JOIN  @resultsSchema.@prefiXetcohort AS p
+    FROM  @resultsSchema.@prefiXevents e
+        INNER JOIN  @resultsSchema.@prefiXetcohort p
             ON e.cohort_id = p.cohort_id;
+
+INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES (CONCAT('..done. There are ',CAST((SELECT COUNT(*) FROM @resultsSchema.@prefiXevents_cohort) AS VARCHAR),' rows in @resultsSchema.@prefiXevents_cohort'));
 
 
 ------------------------------------------------------------------------------------
@@ -257,6 +277,9 @@ SELECT dgn,
 INTO @resultsSchema.@prefiXevent_counts
 FROM @resultsSchema.@prefiXevents_cohort
 GROUP BY dgn;
+
+INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES (CONCAT('..done. There are ',CAST((SELECT COUNT(*) FROM @resultsSchema.@prefiXevent_counts) AS VARCHAR),' rows in @resultsSchema.@prefiXevent_counts'));
+
 
 ------------------------------------------------------------------------------------
 -- Event pair creation
@@ -336,7 +359,7 @@ IF OBJECT_ID('@resultsSchema.@prefiXpairs', 'U') IS NOT NULL
 -- CREATE INDEX @prefiXpairs_d2_idx ON @resultsSchema.@prefiXpairs(event2_concept_id);
 -- CREATE INDEX @prefiXpairs_d1d2_idx ON o@resultsSchema.@prefiXpairs(event1_concept_id,event2_concept_id);
 
-
+INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES (CONCAT('..done. There are ',CAST((SELECT COUNT(*) FROM @resultsSchema.@prefiXpairs) AS VARCHAR),' rows in @resultsSchema.@prefiXpairs'));
 
 ---------------------------------------------------------------------
 -- Create separate table for event pairs that are going to be analysed
@@ -366,6 +389,9 @@ IF OBJECT_ID('@resultsSchema.@prefiXD1D2_model', 'U') IS NOT NULL
 
 DELETE FROM @resultsSchema.@prefiXD1D2_model where EVENT1_EVENT2_COHORT_COUNT< @minPatientsPerEventPair; -- Minimum limit
 -- It is correct to not delete ther corresponding rows from @resultsSchema.@prefiXpairs as these rows can still be used for controls
+
+INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES (CONCAT('..done. There are ',CAST((SELECT COUNT(*) FROM @resultsSchema.@prefiXD1D2_model) AS VARCHAR),' rows in @resultsSchema.@prefiXD1D2_model'));
+
 
 ALTER TABLE @resultsSchema.@prefiXD1D2_model ADD EVENT1_NAME VARCHAR(255) NULL;
 ALTER TABLE @resultsSchema.@prefiXD1D2_model ADD EVENT1_DOMAIN VARCHAR(20) NULL;
@@ -433,6 +459,8 @@ IF OBJECT_ID('@resultsSchema.@prefiXd1d2_summary', 'U') IS NOT NULL
     GROUP BY a.event1_concept_id, a.event2_concept_id, gender, age, discharge_time
     ORDER BY event1_concept_id, event2_concept_id, gender, age, discharge_time;
 
+INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES (CONCAT('..done. There are ',CAST((SELECT COUNT(*) FROM @resultsSchema.@prefiXd1d2_summary) AS VARCHAR),' rows in @resultsSchema.@prefiXd1d2_summary'));
+
 
 ---------------------------------------------------------------------
 -- Create gender,age,discharge-time summary table for all event pairs
@@ -454,6 +482,10 @@ IF OBJECT_ID('@resultsSchema.@prefiXsummary', 'U') IS NOT NULL
     FROM @resultsSchema.@prefiXpairs
     GROUP BY gender, age, discharge_time
     ORDER BY gender, age, discharge_time;
+
+INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES (CONCAT('..done. There are ',CAST((SELECT COUNT(*) FROM @resultsSchema.@prefiXsummary) AS VARCHAR),' rows in @resultsSchema.@prefiXsummary'));
+
+
 
 -------------------------------------------------------
 
@@ -478,6 +510,8 @@ IF OBJECT_ID('@resultsSchema.@prefiXd1_summary', 'U') IS NOT NULL
     INNER JOIN (SELECT DISTINCT event1_concept_id FROM @resultsSchema.@prefiXD1D2_model) b ON a.event1_concept_id=b.event1_concept_id -- limit the table to event pairs only that are going to be analyzed
     GROUP BY a.event1_concept_id, gender, age, discharge_time;
 
+INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES (CONCAT('..done. There are ',CAST((SELECT COUNT(*) FROM @resultsSchema.@prefiXd1_summary) AS VARCHAR),' rows in @resultsSchema.@prefiXd1_summary'));
+
 
 ---------------------------------------------------------------------
 -- Create gender,age,discharge-time summary table for event2_concept_id
@@ -499,5 +533,4 @@ IF OBJECT_ID('@resultsSchema.@prefiXd2_summary', 'U') IS NOT NULL
     INNER JOIN (SELECT DISTINCT event2_concept_id FROM @resultsSchema.@prefiXD1D2_model) b ON a.event2_concept_id=b.event2_concept_id -- limit the table to event pairs only that are going to be analyzed
     GROUP BY a.event2_concept_id, gender, age, discharge_time;
 
--------------------------------------------------------
-
+INSERT INTO @resultsSchema.@prefiXdebug (entry) VALUES (CONCAT('..done. There are ',CAST((SELECT COUNT(*) FROM @resultsSchema.@prefiXd2_summary) AS VARCHAR),' rows in @resultsSchema.@prefiXd2_summary'));

man/createTrajectoryLocalArgs.Rd
@@ -38,7 +38,7 @@ createTrajectoryLocalArgs(
 
 \item{cohortId}{ID of the cohort (in cohortTable) that will be used for the analysis (default value is 1)}
 
-\item{inputFolder}{Full path to input folder that contains SQL file for cohort definition (SQL Server format) and optionally also trajectoryAnalysisArgs.json. You can use built-in folders of this package such as: inputFolder=system.file("extdata", "RA", package = "Trajectories") which is also the default value}
+\item{inputFolder}{Full path to input folder that contains SQL file for cohort definition (SQL Server format) and optionally also trajectoryAnalysisArgs.json. You can use built-in folders of this package such as: inputFolder=system.file("extdata", "RA", package = "Trajectories") which is also the default value. In case your cohort data already exists in the database and you do not need to build it from scratch, set the value to FALSE.}
 
 \item{mainOutputFolder}{The output folder path. This is the folder where the final results are produced into. Use full path and do NOT add trailing slash! The folder must already exist. Default value is the default working directory.}
 