9967c0b227dc3ef5b225b94a82102c719577fbaf
Sulev Reisberg <sulev.reisberg@ut.ee>
2021-08-27 11:57:22 +0300

    A test added

--- tests/testthat/test-2-createEventPairsTable.R
+++ tests/testthat/test-2-createEventPairsTable.R
@@ -100,3 +100,59 @@ testthat::test_that("Filling in cohort table with fulldb buit-in study (there ar
 
 })
 
+
+testthat::test_that("Test that in case of no significant order between E1 and E2, both E1->E2 and E2->E1 are still added in model table for testing", {
+
+  eunomia <-setUpEunomia()
+  connection<-eunomia$connection
+  trajectoryLocalArgs<-eunomia$trajectoryLocalArgs
+  clearConditionsTable(connection)
+  limitToNumPatients(connection,n=100) #in analysis, use 100 patients
+  limitToConcepts(connection)
+  setObservationPeriodForAll(connection,startdate='2010-01-01',enddate='2012-12-31')
+  person_ids<-addConditionEventTrajectory(connection,event_concept_ids=c(317009,255848),n=20,days_to_skip_from_obs_period_start=365) # Add asthma->pneumonia pair for 20 patients
+  person_ids<-addConditionEventTrajectory(connection,event_concept_ids=c(255848,317009),n=20,days_to_skip_from_obs_period_start=365) # Add pneumonia->asthma pair for 20 patients
+  addRandomEvents(connection,n_per_person_range=c(0,10),exclude_concept_ids=c(317009,255848)) # Add up to 10 random events per each patient, except events 317009 and 255848
+
+
+  trajectoryAnalysisArgs <- Trajectories::createTrajectoryAnalysisArgs(minimumDaysBetweenEvents = 1,
+                                                                       maximumDaysBetweenEvents = 365*120,
+                                                                       minPatientsPerEventPair = 10,
+                                                                       addConditions=T,
+                                                                       addObservations=F,
+                                                                       addProcedures=F,
+                                                                       addDrugExposures=F, # NB! DO NOT USE BOTH addDrugEras=T and addDrugExposures=T (not both) as it leads to analysis duplication and breaks some code... (same "drug" event may occur several times which is not allowed)
+                                                                       addDrugEras=F, # NB! DO NOT USE BOTH addDrugEras=T and addDrugExposures=T (not both) as it leads to analysis duplication and breaks some code... (same "drug" event may occur several times which is not allowed)
+                                                                       addBirths=F,
+                                                                       addDeaths=F,
+                                                                       daysBeforeIndexDate=Inf,
+                                                                       cohortName="test")
+
+
+  #Create output folder for this analysis
+  outputFolder<-Trajectories::GetOutputFolder(trajectoryLocalArgs,trajectoryAnalysisArgs,createIfMissing=T)
+
+  #Remove output files (if exist from previous run)
+  removeTestableOutputFiles(trajectoryLocalArgs,trajectoryAnalysisArgs)
+
+  # Create new cohort table for this package to results schema & fill it in (all having cohort_id=1 in cohort data)
+  Trajectories::createAndFillCohortTable(connection=connection,
+                                         trajectoryAnalysisArgs=trajectoryAnalysisArgs,
+                                         trajectoryLocalArgs=trajectoryLocalArgs)
+
+  # Create database tables of all event pairs (patient level data + summary statistics)
+  Trajectories::createEventPairsTable(connection=connection,
+                                      trajectoryAnalysisArgs=trajectoryAnalysisArgs,
+                                      trajectoryLocalArgs=trajectoryLocalArgs)
+
+
+  # Get number of rows from E1E2_model table (should be >10 rows)
+  res<-querySql(connection, glue::glue('SELECT * FROM {trajectoryLocalArgs$resultsSchema}.{trajectoryLocalArgs$prefixForResultTableNames}E1E2_model'))
+  rows.E1.E2<-res %>% filter(E1_CONCEPT_ID==317009 & E2_CONCEPT_ID==255848) %>% nrow()
+  rows.E2.E1<-res %>% filter(E1_CONCEPT_ID==255848 & E2_CONCEPT_ID==317009) %>% nrow()
+  testthat::expect_equal(rows.E1.E2, 1)
+  testthat::expect_equal(rows.E2.E1, 1)
+
+
+})
+
