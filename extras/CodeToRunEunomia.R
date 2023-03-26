94694262bbbf5f8faf874bb7e530f9cb63d0a80b
Samir Musali <samir.musali@mail.com>
2023-03-27 00:08:53 +0300

    update: introduce CohortGenerator
    This commit does the followings:
    - introduces CohortGenerator
    - removes createCohortTable and fillCohortTable functions
    - updates DESCRIPTION

--- extras/CodeToRunEunomia.R
+++ extras/CodeToRunEunomia.R
@@ -25,14 +25,17 @@ Eunomia::createCohorts(connectionDetails)
 
 # Setting database parameters:
 library(stringi)
-mainOutputFolder <- readline(prompt = "Main Output Folder: ")
+mainOutputFolder <- file.path("~", "temp")
+dir.create(mainOutputFolder, recursive = TRUE, showWarnings = FALSE) # Create directory if it does not exist
 trajectoryLocalArgs <- Trajectories::createTrajectoryLocalArgs(oracleTempSchema = "temp_schema",
                                                                prefixForResultTableNames = "",
                                                                cdmDatabaseSchema = 'main',
                                                                vocabDatabaseSchema = 'main',
                                                                resultsSchema = 'main',
                                                                sqlRole = F,
-                                                               inputFolder=system.file("extdata", "fulldb", package = "Trajectories"), # Full path to input folder that contains SQL file for cohort definition and optionally also trajectoryAnalysisArgs.json. You can use built-in folders of this package such as: inputFolder=system.file("extdata", "T2D", package = "Trajectories")
+                                                               # Full path to input folder that contains SQL file for cohort definition and optionally also trajectoryAnalysisArgs.json.
+                                                               # You can use built-in folders of this package such as: inputFolder=system.file("extdata", "T2D", package = "Trajectories")
+                                                               inputFolder=system.file("extdata", "Hypertension", package = "Trajectories"),
                                                                mainOutputFolder=mainOutputFolder, # Subfolders to this will be created automatically
                                                                databaseHumanReadableName='Eunomia')
 
@@ -43,12 +46,12 @@ trajectoryLocalArgs <- Trajectories::createTrajectoryLocalArgs(oracleTempSchema 
 
 Trajectories::discover(connection,
                        trajectoryLocalArgs,
-                       createCohort=F,
-                       validationSetSize=0,
-                       createEventPairsTable=T,
+                       createCohort=T,
+                       validationSetSize=1,
+                       createEventPairsTable=F,
                        runDiscoveryAnalysis=F,
-                       createFilteredFullgraphs=T,
-#                       createGraphsForSelectedEvents = F, # Complains as if it is unused argument
+                       createFilteredFullgraphs=F,
+                       createGraphsForSelectedEvents=F,
                        selfValidate=F,
                        cleanup=T)
 
@@ -57,4 +60,3 @@ Trajectories::discover(connection,
 # ##################################################
 
 DatabaseConnector::disconnect(connection)
-
